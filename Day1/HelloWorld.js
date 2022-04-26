f0d1bdc102160e50491c0589a1170453efd321b6
cccc <905141352@qq.com>
2022-04-26 16:50:53 +0800

    feat: 更新day1

--- Day1/HelloWorld.js
+++ Day1/HelloWorld.js
@@ -1,28 +1,29 @@
+// 控制台打印数据
 console.log('hello world!')
-var http = require("http");
-http.createServer(function (request, response) {
+
+// http内置API
+const http = require('http');
+
+// 主机IP
+const hostname = '127.0.0.1';
+// 端口号
+const port = 3000;
+
+// 创建服务器 通过 request, response 参数来接收和响应数据。
+const server = http.createServer((req, res) => {
 
     // 发送 HTTP 头部
     // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
     res.statusCode = 200;
+    // 设置头部: 内容类型: text/plain
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    // 也可以合并写为response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    //相应数据到终端页面(doc)
+    res.end('Hello, World!\n');
 });
 
+// listen 方法绑定主机和端口
 server.listen(port, hostname, () => {
-    console.log(`服务器运行在 http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
